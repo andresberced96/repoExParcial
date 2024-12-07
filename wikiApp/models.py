8d96884daf3a5914ec721224e7a2807c5042fe65
Andres Bernaola <pepe_96_123@hotmail.com>
2024-12-07 10:38:48 -0500

    version 2.0 incluye modificación de vistas nuevo tema y nuevo artículo y BD

--- wikiApp/models.py
+++ wikiApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class temaWiki(models.Model):
+    nombre_tema = models.CharField(max_length=128, null=True, blank=True)
+    descripcion_tema = models.CharField(max_length=512, null=True, blank=True)
+
+class articuloWiki(models.Model):
+    titulo = models.CharField(max_length=128)
+    contenido = models.CharField(max_length=1024)
+    tema_relacionado = models.ForeignKey(temaWiki, null=True, blank=True, on_delete=models.CASCADE)
--- wikiApp/views.py
+++ wikiApp/views.py
@@ -1,12 +1,38 @@
-
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import temaWiki,articuloWiki
+
+temas = temaWiki.objects.all()
 
 def principal(request):
-    return render(request, 'principal.html')
+    return render(request,'principal.html',{
+        'temas':temas
+    })
 
 def nuevo_tema(request):
-    return render(request, 'nuevo_tema.html')
+    if request.method == 'POST':
+        nombre_tema = request.POST.get('nombre_tema')
+        descripcion_tema = request.POST.get('descripcion_tema')
+        temaWiki.objects.create(
+            nombre_tema=nombre_tema,
+            descripcion_tema=descripcion_tema
+        )
+    return render(request, 'nuevo_tema.html',{
+        'temas':temas
+        })
 
 def nuevo_articulo(request):
-    return render(request, 'nuevo_articulo.html')
+    if request.method == 'POST':
+        titulo = request.POST.get('titulo')
+        contenido = request.POST.get('contenido')
+        tema_relacionado = request.POST.get('tema_relacionado')
+        temaObj = temaWiki.objects.get(id=tema_relacionado)
+        temaWiki.objects.create(
+            titulo = titulo,
+            contenido = contenido,
+            tema_relacionado = temaObj
+        )    
+    return render(request, 'nuevo_articulo.html',{
+        'temas':temas
+        })
